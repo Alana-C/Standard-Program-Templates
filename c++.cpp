6b2cf66447821bda779776fc05a04acd9340cf63
Alana-C <ac2249@byu.edu>
2024-06-04 15:55:10 -0600

    Updated c++

--- c++.cpp
+++ c++.cpp
@@ -8,20 +8,47 @@ comment
 
 //at the top, to allow input and output
 #include <iostream>
+//for math
+#include <cmath>
+//standard
+#include <cstdlib>
+
 using namespace std;
 
 //the main program, what is executed
 int main() {
     //each statement ends with semicolon
     //assign a variable, with a type
-    int wage;
-    wage = 20;
+    double wage;
+    wage = 20.0;
+
+
+    //const for constants, all caps and underscores
+    const int HOURS_PER_WEEK = 40;
 
     //cin "characters in" to get input from keyboard, >> to assign to variable
     cin >> wage;
 
+    int a = wage;
+
+    switch(a) {
+        case 0:
+            cout << "Get a job!" << endl;
+            break;
+        default:
+            break;
+    }
+
+    if (wage <= 12.0) {
+        cout << "Ouch." << endl;
+    }
+    else if (wage >= 60) {
+        cout << "Nice." << endl;
+    }
+
     //cout to output or 'print', no automatic end line, << concatenates
-    cout << "Salary is " << wage * 40 * 52;
+    cout << "Salary is " << wage * HOURS_PER_WEEK * 52 << endl;
+    cout << "Monthly Salary is " << wage * HOURS_PER_WEEK * 52 / 12 << endl;
     //endl (or "/n") is enter
     cout << endl;
 
@@ -29,10 +56,16 @@ int main() {
     return 0;
 }
 
+// Notes
+    // (static_cast<type>(expression)) will convert variable type
+    // int/int results in int, e.g. 3/5 -> 1
+    // char 'c' string "string" 
+    // names are only valid within their scope
+
 // Compiling
     // command line, in your program's folder
     // standard:
-    // g++ -o yourfile.cpp
+    // g++ -o name-of-executable yourfile.cpp
     // to show warnings:
     // g++ -Wall yourfile.cpp
 
